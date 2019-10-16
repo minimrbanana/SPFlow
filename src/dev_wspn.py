15643a59c8bbf12156cdc1bd78711457a756e407
yu <yzjyzj88212@sina.com>
2019-10-16 18:43:50 +0200

    add S&P training

--- src/dev_wspn.py
+++ src/dev_wspn.py
@@ -70,17 +70,21 @@ def learn_whittle_spn_2d(train_data, n_RV, n_min_slice, init_scope=None):
     print('learning WSPN')
     # need to pair RVs
     # need flag for 2d?
-    if ARGS.data_type==1:
-        l_rfft = 17
-    else:
+    if ARGS.data_type==2:
         l_rfft = 8
+    else:
+        l_rfft = 17
     # l_rfft!=None --> 2d/pair gaussian node, is_pair=False --> 2d gaussian, diagonal covariance matrix
     wspn = learn_parametric(train_data, ds_context, min_instances_slice=n_min_slice, initial_scope=init_scope,
                             cpus=4, l_rfft=l_rfft, is_pair=False)
     if ARGS.data_type==1:
         save_path = './dev/sine/wspn2d_'+str(n_min_slice)+'/'
-    else:
+    elif ARGS.data_type==2:
         save_path = './dev/mnist/wspn2d_'+str(n_min_slice)+'/'
+    elif ARGS.data_type==3:
+        save_path = './dev/SP/wspn2d_' + str(n_min_slice) + '/'
+    else:
+        save_path = './dev/stock/wspn2d_' + str(n_min_slice) + '/'
     check_path(save_path)
     f = open(save_path+'wspn_2d.pkl', 'wb')
     pickle.dump(wspn, f)
@@ -92,8 +96,10 @@ def learn_whittle_spn_2d(train_data, n_RV, n_min_slice, init_scope=None):
 def load_whittle_spn_2d(n_min_slice):
     if ARGS.data_type==1:
         save_path = './dev/sine/wspn2d_'+str(n_min_slice)+'/'
-    else:
+    elif ARGS.data_type==2:
         save_path = './dev/mnist/wspn2d_'+str(n_min_slice)+'/'
+    else:
+        save_path = './dev/SP/wspn2d_'+str(n_min_slice)+'/'
     f = open(save_path+'wspn_2d.pkl', 'rb')
     spn = pickle.load(f)
     f.close()
@@ -173,15 +179,15 @@ if __name__ == '__main__':
     # set parameters
     parser = argparse.ArgumentParser()
     # Args go here
-    parser.add_argument('--wspn_type', type=int, default=1,
+    parser.add_argument('--wspn_type', type=int, default=2,
                         help='Type of wspn, 1-1d, 2-2d, 3-pair')
-    parser.add_argument('--train_type', type=int, default=1,
+    parser.add_argument('--train_type', type=int, default=2,
                         help='Type of train, 1-train, 2-test')
-    parser.add_argument('--n_min_slice', type=int, default=2100,
+    parser.add_argument('--n_min_slice', type=int, default=1100,
                         help='minimum size of slice.')
     parser.add_argument('--data_type', type=int, default=2,
-                        help='Type of data, 1-sine, 2-mnist')
-    parser.add_argument('--threshold', type=float, default=0.3,
+                        help='Type of data, 1-sine, 2-mnist, 3-S&P')
+    parser.add_argument('--threshold', type=float, default=0.8,
                         help='Threshold of splitting features')
 
     ARGS, unparsed = parser.parse_known_args()
@@ -219,6 +225,18 @@ if __name__ == '__main__':
         scope_temp = np.delete(scope_list, np.where(scope_list % 16 == 8))
         init_scope = list(np.delete(scope_temp, np.where(scope_temp % 16 == 15)))
         # data_train = data_train[0:100, :]
+    elif ARGS.data_type==3:
+        print('loading S&P data')
+        data_train = np.fromfile('/media/yu/data/yu/code/gp_whittle/WhittleNetwork/train_SP.dat',
+                                 dtype=np.float64).reshape(-1, 374)
+        data_pos = data_train.copy()
+        data_neg = data_train.copy()
+        n_RV = 374  # number of RVs
+        p = 11  # dim
+        L = 32  # TS length
+        scope_list = np.arange(n_RV)
+        scope_temp = np.delete(scope_list, np.where(scope_list % 34 == 17))
+        init_scope = list(np.delete(scope_temp, np.where(scope_temp % 34 == 33)))
     else:
         sys.exit()
     print('data done')
